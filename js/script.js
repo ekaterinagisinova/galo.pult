7fbc661baaefa5daa05384c4e96d80f467c3fde9
Ekaterina Gisinova <surkatena@mail.ru>
2021-04-05 11:35:14 +0300

    готовы слайды для Slider_2

--- js/script.js
+++ js/script.js
@@ -28,58 +28,6 @@ for (let i = 0; i < goodsItems.length; i++){
     goodsItems[i].addEventListener('mousemove', activeImg);
 }
 
-
-// // -----Slider-----
-
-// const slides = document.querySelectorAll('.slide_01');
-// const paginations =document.querySelectorAll('.paginationItem_01');
-
-// let slideNumber = 0;
-
-// const activeSlide = function(){
-//     for (let slide of slides) {       
-//         slide.classList.remove('activeSlide');
-//     }
-//     slides[slideNumber].classList.add('activeSlide');
-// }
-
-// const activePagination = function(){
-//     for (let pagination of paginations) {       
-//         pagination.classList.remove('activePagination');
-//     }
-//     paginations[slideNumber].classList.add('activePagination');
-// }
-
-// const nextSlide = function() {
-//     if(slideNumber >= slides.length){
-//         slideNumber = 0;              
-//         activePagination();
-//         activeSlide();        
-//     }
-//     else{          
-//         activePagination();
-//         activeSlide();    
-//         slideNumber++;
-//     }
-// } 
-
-// const slideShow = function(event){
-//     if(event == undefined){
-//             nextSlide();
-//         }
-//     else {
-//         slideNumber = event.target.getAttribute('data_pagination');
-//         nextSlide();
-//     }      
-// }
-
-// for (let i = 0; i < paginations.length; i++){
-//     paginations[i].addEventListener('click', slideShow);    
-//     paginations[i].setAttribute('data_pagination', i);
-// }
-
-// setInterval(slideShow, 2000);
-
 // -----Slider-----
 
 const slides_01 = document.querySelectorAll('.slide_01');
@@ -91,23 +39,19 @@ const paginations_02 =document.querySelectorAll('.paginationItem_02');
 let slideNumber_01 = 0;
 let slideNumber_02 = 0;
 
-const activeSlide = function(el,num){  
-    // console.log(el);  
+const activeSlide = function(el,num){        
     for (let slide of el) { 
         slide.classList.remove('activeSlide');
     }
     el[num].classList.add('activeSlide');
 }
 
-const activePagination = function(el, num){
+const activePagination = function(el, num){    
     for (let pagination of el) {  
         pagination.classList.remove('activePagination');
     }
     el[num].classList.add('activePagination');
 }
-
-
-
 
 // для 1го слайдера
 const nextSlide_01 = function(sl, p) {
@@ -122,23 +66,19 @@ const nextSlide_01 = function(sl, p) {
         slideNumber_01++;
     }
 } 
-
 const slideShow_01 = function(event){
     if(event == undefined){
             nextSlide_01(slides_01, paginations_01);
         }
     else {
-        slideNumber = event.target.getAttribute('data_pagination_01');
+        slideNumber_01 = event.target.getAttribute('data_pagination_01');
         nextSlide_01(slides_01, paginations_01);
     }      
 }
-
-
 for (let i = 0; i < paginations_01.length; i++){
     paginations_01[i].addEventListener('click', slideShow_01);    
     paginations_01[i].setAttribute('data_pagination_01', i);
 }
-
 setInterval(slideShow_01, 3000);
 
 
@@ -155,21 +95,17 @@ const nextSlide_02 = function(sl, p) {
         slideNumber_02++;
     }
 } 
-
 const slideShow_02 = function(event){
     if(event == undefined){
             nextSlide_02(slides_02, paginations_02);
         }
     else {
-        slideNumber = event.target.getAttribute('data_pagination_02');
+        slideNumber_02 = event.target.getAttribute('data_pagination_02');        
         nextSlide_02(slides_02, paginations_02);
     }      
 }
-
-
 for (let i = 0; i < paginations_02.length; i++){
     paginations_02[i].addEventListener('click', slideShow_02);    
     paginations_02[i].setAttribute('data_pagination_02', i);
 }
-
 setInterval(slideShow_02, 2000);
